0fbd94c4933e34534bed02771b1bebd7924daf95
Tomasz Welman <welmantw@amazon.com>
2019-07-07 15:32:40 +0100

    next

---
+++ desanitize.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -
+test -f server.py.orig && mv server.py.orig server.py
+test -f yup_notify.cs.orig && mv yup_notify.cs.orig yup_notify.cs
--- yup_notify.cs
+++ yup_notify.cs
@@ -9,14 +9,18 @@ using System.Security.Authentication;
 
 public class YupNotify
 {
-  private static readonly String API_KEY = "abc123";
-  private static readonly String ENDPOINT_URI = "https://127.0.0.1:8000/notify";
+  private const String ENV_NOTIFICATION = "YUP_NOTIFY_NOTIFICATION";
+  private const String ENV_API_KEY = "YUP_NOTIFY_API_KEY";
+  private const String ENV_ENDPOINT_URI = "YUP_NOTIFY_ENDPOINT_URI";
+  private static String API_KEY = "abc123";
+  private static String ENDPOINT_URI = "https://127.0.0.1:8000/notify";
+  private static String NOTIFICATION = "uploaded";
 
   private async Task<String> sendPostRequest(String uri) {
     var values = new Dictionary<string, string>
       {
         { "key", API_KEY },
-        { "notification", "uploaded" }
+        { "notification", NOTIFICATION }
       };
 
     var content = new FormUrlEncodedContent(values);
@@ -32,8 +36,8 @@ public class YupNotify
 
     return null;
   }
-
-  private static String ExtractCertParam(string container, string paramName) {
+/*
+private static String ExtractCertParam(string container, string paramName) {
     foreach (var param in container.Split(',')) {
       var p = param.Trim();
       var kv = p.Split('=');
@@ -44,14 +48,43 @@ public class YupNotify
 
     return null;
   }
+*/
+  private static String GetEnvVar(string name) {
+    var value = Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+    if (null == value) {
+      value = Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.User);
+      if (null == value) {
+        value = Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Machine);
+      }
+    }
+
+    return value;
+  }
+
+  private static void ReadEnvironment() {
+    var value = GetEnvVar(ENV_NOTIFICATION);
+    if (null != value) {
+      NOTIFICATION = value;
+    }
+    value = GetEnvVar(ENV_API_KEY);
+    if (null != value) {
+      API_KEY = value;
+    }
+    value = GetEnvVar(ENV_ENDPOINT_URI);
+    if (null != value) {
+      ENDPOINT_URI = value;
+    }
+  }
 
   public static int Main(string[] args) {
+    ReadEnvironment();
     ServicePointManager.ServerCertificateValidationCallback = (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) =>
     {
       //Console.WriteLine(ExtractCertParam(certificate.Subject, "CN"));
 
-      return true; //accepts invalid certificates
+      return true; //accepts also invalid certificates
     };
+
     ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
     var endpoint_uri = ENDPOINT_URI;
