d85e8c580c83ccf3e514f2b1f231b1e8c6925b40
Trogious <trog@swmud.net>
2020-02-25 11:40:55 +0000

    upload support

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh -
+test -f rsync.cs && mcs -r:System.Net.Http -sdk:4.5 rsync.cs && mono rsync.exe $@ ; rm -f ./rsync.exe
 mcs -r:System.Net.Http -sdk:4.5 yup_notify.cs && mono yup_notify.exe $@
--- yup_notify.cs
+++ yup_notify.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Collections.Generic;
@@ -9,14 +11,22 @@ using System.Security.Authentication;
 
 public class YupNotify
 {
-  private const String ENV_NOTIFICATION = "YUP_NOTIFY_NOTIFICATION";
-  private const String ENV_API_KEY = "YUP_NOTIFY_API_KEY";
-  private const String ENV_ENDPOINT_URI = "YUP_NOTIFY_ENDPOINT_URI";
-  private static String API_KEY = "abc123";
-  private static String ENDPOINT_URI = "https://127.0.0.1:8000/notify";
-  private static String NOTIFICATION = "gameplay";
+  private const string ENV_NOTIFICATION = "YUP_NOTIFY_NOTIFICATION";
+  private const string ENV_API_KEY = "YUP_NOTIFY_API_KEY";
+  private const string ENV_ENDPOINT_URI = "YUP_NOTIFY_ENDPOINT_URI";
+  private const string ENV_SSH_PORT = "YUP_NOTIFY_SSH_PORT";
+  private const string ENV_RSYNC_DESTINATION = "YUP_NOTIFY_RSYNC_DESTINATION";
+  private static readonly string[] VIDEO_EXTENSIONS = new string[] {"mp4", "mkv", "avi", "mov", "m2ts"};
+  private static readonly string[] RSYNC_PATHS = new string[] {@".\rsync.exe", @"C:\Program Files\cwrsync\bin\rsync.exe", @"C:\Program Files (x86)\cwrsync\bin\rsync.exe", @"C:\ProgramData\cwrsync\bin\rsync.exe",
+                                                     @"C:\Windows\System32\rsync.exe", @"C:\Windows\System\rsync.exe", @"C:\Windows\rsync.exe", @"C:\rsync.exe", "./rsync", "/usr/local/bin/rsync", "/usr/bin/rsync", "/bin/rsync"};
+  private static string API_KEY = "abc123";
+  private static string ENDPOINT_URI = "https://127.0.0.1:8000/notify";
+  private static string NOTIFICATION = "gameplay";
+  private static int SSH_PORT = 22;
+  private static string RSYNC_DESTINATION = "yup@127.0.0.1:dest_dir/";
+  private static string exeContainingDir;
 
-  private async Task<String> sendPostRequest(String uri) {
+  private async Task<string> sendPostRequest(string uri) {
     var values = new Dictionary<string, string>
       {
         { "key", API_KEY },
@@ -37,7 +47,7 @@ public class YupNotify
     return null;
   }
 
-  private static String ExtractCertParam(string container, string paramName) {
+  private static string ExtractCertParam(string container, string paramName) {
       foreach (var param in container.Split(',')) {
         var p = param.Trim();
         var kv = p.Split('=');
@@ -49,7 +59,7 @@ public class YupNotify
       return null;
     }
 
-  private static String GetEnvVar(string name) {
+  private static string GetEnvVar(string name) {
     var value = Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
     if (null == value) {
       value = Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.User);
@@ -74,10 +84,89 @@ public class YupNotify
     if (null != value) {
       ENDPOINT_URI = value;
     }
+    value = GetEnvVar(ENV_SSH_PORT);
+    if (null != value) {
+      SSH_PORT = Int32.Parse(value);
+    }
+    value = GetEnvVar(ENV_RSYNC_DESTINATION);
+    if (null != value) {
+      RSYNC_DESTINATION = value;
+    }
+  }
+
+  private static string GetExeContainingDir() {
+    if (exeContainingDir == null) {
+      exeContainingDir =  Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+    }
+    return exeContainingDir;
+  }
+
+  private static string GetRsyncPath() {
+    foreach (var path in RSYNC_PATHS) {
+      // Console.WriteLine("raw paths:");
+      if (File.Exists(path)) {
+        return path;
+      } else if (path.StartsWith(@".\") || path.StartsWith("./")) {
+        string fullPath = Path.GetFullPath(Path.Combine(GetExeContainingDir(), path.Substring(2)));
+        // Console.WriteLine("full path: " + fullPath);
+        if (File.Exists(fullPath)) {
+          return fullPath;
+        }
+      }
+    }
+
+    return null;
+  }
+
+  private static List<string> GetVideoFilePaths() {
+    List<string> videoPaths = new List<string>(Directory.GetFiles(GetExeContainingDir(), "*." + VIDEO_EXTENSIONS[0], SearchOption.TopDirectoryOnly));
+    for (var i = 1; i < VIDEO_EXTENSIONS.Length; ++i) {
+      videoPaths.AddRange(Directory.GetFiles(GetExeContainingDir(), "*." + VIDEO_EXTENSIONS[i], SearchOption.TopDirectoryOnly));
+    }
+
+    return videoPaths;
+  }
+
+  private static int ExecuteCommand(string workingDir, string exePath, IEnumerable<string> args) {
+    var startInfo = new ProcessStartInfo();
+    // startInfo.UseShellExecute = true;
+    startInfo.WorkingDirectory = workingDir;
+
+    startInfo.FileName = exePath;
+    // startInfo.Verb = "runas";
+    startInfo.Arguments = "-hPe 'ssh -p" + SSH_PORT + "' " + string.Join(" ", args) + " " + RSYNC_DESTINATION;
+    // startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+    Console.WriteLine(startInfo.FileName + " " + startInfo.Arguments);
+    Process proc = Process.Start(startInfo);
+    proc.WaitForExit();
+    return proc.ExitCode;
   }
 
   public static int Main(string[] args) {
     ReadEnvironment();
+
+    string workingDir = GetExeContainingDir();
+    Console.WriteLine("working directory: " + workingDir);
+    string rsyncPath = GetRsyncPath();
+    if (rsyncPath == null) {
+      Console.WriteLine("no rsync binary found");
+      return 3;
+    }
+
+    var videos = GetVideoFilePaths();
+    if (videos.Count < 1) {
+      Console.WriteLine("no videos found");
+      return 0;
+    }
+    Console.WriteLine("videos to upload from {0}:", workingDir);
+    videos.ForEach(v => Console.WriteLine(" " + Path.GetFileName(v)));
+
+    int code = ExecuteCommand(workingDir, rsyncPath, videos);
+    if (code != 0) {
+      Console.WriteLine("non-zero return code from rsync: {0}", code);
+      return code;
+    }
+
     ServicePointManager.ServerCertificateValidationCallback = (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) =>
     {
       //Console.WriteLine(ExtractCertParam(certificate.Subject, "CN"));
